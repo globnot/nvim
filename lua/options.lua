43140b73f05ee0d3d74f990cf7fc622ed415c779
Antoine Borda <pro@antoineborda.com>
2025-08-01 11:51:50 +0200

    [SETUP] Initialization

---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.opt.expandtab = false    -- IMPORTANT : garder les tabulations, ne pas convertir en espaces
+vim.opt.tabstop = 4          -- largeur visuelle d'une tabulation = 4 espaces
+vim.opt.shiftwidth = 4       -- indentation automatique = 4 (en tabulations)
+vim.opt.softtabstop = 4      -- indentation quand tu appuies sur Tab = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
